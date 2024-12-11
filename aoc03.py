b48b3e74968c988f5380c495d8d7dd9a7a2d9308
Jürgen Strobel <juergen+github@strobel.info>
2024-12-11 21:57:19 +0100

    fix aoc03 formatting

--- aoc03.py
+++ aoc03.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+# ruff: noqa: E701 E702
 
 import re
 
 from aoc_util import run_aoc
 
-def aoc03(input):
 
+def aoc03(input):
     def muls(m):
         return int(m[1]) * int(m[2])
 
@@ -14,15 +15,17 @@ def aoc03(input):
 
     def muls2(m):
         nonlocal flag
+        # fmt: off
         match m[1] or m[2]:
             case "do()":       flag = 1; return 0
             case "don't()":    flag = 0; return 0
             case _:            return flag * int(m[3]) * int(m[4])
+        # fmt: on
 
     mulsre2 = re.compile(r"(do\(\))|(don\'t\(\))|mul\((\d{1,3}),(\d{1,3})\)")
     flag = 1
     yield sum(map(muls2, mulsre2.finditer(input)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_aoc(aoc03)
