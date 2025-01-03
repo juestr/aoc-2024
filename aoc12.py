bda3dacc109e039a5d1c19cca0739d68bab8811f
Jürgen Strobel <juergen+github@strobel.info>
2025-01-03 16:58:04 +0100

    Double performance day 12

--- aoc12.py
+++ aoc12.py
@@ -6,27 +6,25 @@ from scipy import ndimage, signal
 from aoc_util import np_raw_table, run_aoc
 
 EDGE = np.array([[1, -1]], dtype="int8")
-EDGES = [np.rot90(EDGE, r) for r in range(4)]
 STRAIGHT_EDGE = np.array([[-1, -1], [1, 1]], dtype="int8")
-STRAIGHT_EDGES = [np.rot90(STRAIGHT_EDGE, r) for r in range(4)]
-
-
-def count_patterns(xs, patterns, threshold):
-    return sum(
-        np.sum(signal.correlate(xs, pattern, mode="full") >= threshold)
-        for pattern in patterns
-    )
 
 
 def aoc12(garden):
+    def count_pattern(xs, pattern, threshold):
+        return sum(
+            np.sum(np.abs(signal.correlate(xs, p, mode="full")) >= threshold)
+            for p in (pattern, pattern.T)
+        )
+
     def plant_costs(plant):
+        # return array of shape (2,) holding results for both parts
         regions, n = ndimage.label(garden == plant)
         return sum(region_costs(regions == i) for i in range(1, n + 1))
 
     def region_costs(region):
         area = np.sum(region)
-        fence = count_patterns(region, EDGES, 1)
-        straight_edges = count_patterns(region, STRAIGHT_EDGES, 2)
+        fence = count_pattern(region, EDGE, 1)
+        straight_edges = count_pattern(region, STRAIGHT_EDGE, 2)
         return np.array([area * fence, area * (fence - straight_edges)])
 
     yield from sum(map(plant_costs, np.unique(garden)))
